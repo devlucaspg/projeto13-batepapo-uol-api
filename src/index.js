218fb54d994e5b0a8698cb64924a60e677043e26
devlucaspg <110506094+devlucaspg@users.noreply.github.com>
2022-11-10 03:39:14 -0400

    POST /participants

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import dotenv from 'dotenv'
+import cors from 'cors';
+import joi from 'joi';
+import { MongoClient } from 'mongodb';
+
+dotenv.config();
+
+const app = express();
+
+const mongoClient = new MongoClient(process.env.MONGO_URL);	// { useUnifiedTopology: true }
+let db
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {console.log('Server is running in port: 5000')});
+
+try {
+	await mongoClient.connect();
+	db = mongoClient.db("projeto13-batepapo-uol-api");
+} catch (err) {
+	console.log(err);
+}
+
+app.post ('/participants', async (req, res) => {
+
+	const { name } = req.body;
+
+	const schema = joi.object({
+		name: joi.string().min(1).required()
+	});
+
+	const validation = schema.validate(req.body);
+
+	if (validation.error) {
+		return res.sendStatus(422);
+	}
+
+	const participant = await db.collection("participants").findOne({name});
+
+	if (participant) {
+		return res.sendStatus(409);
+	}
+
+	await db.collection("participants").insertOne({name, lastStatus: Date.now()});
+
+	res.sendStatus(201);
+});
