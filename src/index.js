324bd49beebcc2ef34f469c5a2f13f396547d29c
devlucaspg <110506094+devlucaspg@users.noreply.github.com>
2022-11-11 02:38:57 -0400

    GET /messages

--- src/index.js
+++ src/index.js
@@ -9,44 +9,53 @@ dotenv.config();
 
 const app = express();
 
+const sParticipants = joi.object({
+	name: joi.string().min(1).required()
+});
+
+const sMessages = joi.object({
+	to: joi.string().min(1).required(),
+	text: joi.string().min(1).required(),
+	type: joi.string().required()
+});
+
 const mongoClient = new MongoClient(process.env.MONGO_URL);	// { useUnifiedTopology: true }
 let db
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(5000, () => {console.log('Server is running in port: 5000')});
-
 try {
 	await mongoClient.connect();
+	console.log("MongoDB Connected");
 	db = mongoClient.db("projeto13-batepapo-uol-api");
 } catch (err) {
 	console.log(err);
-	sendStatus(500);
 }
 
 app.post ('/participants', async (req, res) => {
 
 	const { name } = req.body;
 
-	const schema = joi.object({
-		name: joi.string().min(1).required()
-	});
-
-	const validation = schema.validate(req.body);
+	const validation = sParticipants.validate(req.body);
 
 	if (validation.error) {
 		return res.sendStatus(422);
 	}
 
-	const participant = await db.collection("participants").findOne({name});
+	try {
+		const participant = await db.collection("participants").findOne({name});
 
-	if (participant) {
-		return res.sendStatus(409);
+		if (participant) {
+			return res.sendStatus(409);
+		}
+	} catch (err) {
+		console.log(err);
+		return res.sendStatus(500);
 	}
 
 	try {
-		await db.collection("participants").insert({name, lastStatus: Date.now()});
+		await db.collection("participants").insertOne({name, lastStatus: Date.now()});
 		res.sendStatus(201);
 	} catch (err) {
 		res.sendStatus(500);
@@ -58,6 +67,7 @@ app.get ('/participants', async (req, res) => {
 		const participants = await db.collection("participants").find().toArray();
 		res.send(participants);
 	} catch (err) {
+		console.log(err);
 		res.sendStatus(500);
 	}
 });
@@ -67,13 +77,7 @@ app.post ('/messages', async (req, res) => {
 	const { to, text, type } = req.body;
 	const from = req.headers.user;
 
-	const schema = joi.object({
-		to: joi.string().min(1).required(),
-		text: joi.string().min(1).required(),
-		type: joi.string().required()
-	});
-
-	schema.validate(req.body);
+	sMessages.validate(req.body);
 	
 	try{
 		const participant = await db.collection("participants").findOne({name: from});
@@ -83,17 +87,26 @@ app.post ('/messages', async (req, res) => {
 			return res.sendStatus(201);
 		} 
 	} catch (err) {
+		console.log(err);
 		res.sendStatus(500);
 	}	
 });
 
-/* app.get ('/messages', async (req, res) => {
-	try{
+app.get ('/messages', async (req, res) => {
+	const limit = parseInt(req.query.limit);
+	const user = req.headers.user;
 
+	try{
+		const allMessages = await db.collection("messages").find().toArray();
+		const messages = allMessages
+			.filter((message) => message.to === user || message.from === user || message.to === "Todos" || message.type === "message")
+			.slice(-limit);
+		res.send(messages);
 	} catch (err) {
+		console.log(err);
 		res.sendStatus(500);
 	}
-}); */
+});
 
 app.post ('/status', async (req, res) => {
 	const { user } = req.headers;
@@ -112,13 +125,23 @@ app.post ('/status', async (req, res) => {
 
 async function removeInactiveParticipants() {
 	const time = Date.now() - 10000;
-	const users = await db.collection("participants").find({lastStatus: {$lt: time}}).toArray();
 
-	for (let i = 0; i < users.length; i++) {
-		await db.collection("messages").insert({from: users[i].name, to: "Todos", text: "sai da sala...", type: "status", time: dayjs().format('HH:mm:ss')});
+	try {
+		const users = await db.collection("participants").find({lastStatus: {$lt: time}}).toArray();
+
+		for (let i = 0; i < users.length; i++) {
+			await db.collection("messages").insertOne({from: users[i].name, to: "Todos", text: "sai da sala...", type: "status", time: dayjs().format('HH:mm:ss')});
+		}
+
+		db.collection("participants").deleteMany({lastStatus: {$lt: time}});
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
 	}
-	
-	db.collection("participants").deleteMany({lastStatus: {$lt: time}});
 }
 
 setInterval(removeInactiveParticipants, 15000);
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server is running in port: ${process.env.PORT}`)
+});
